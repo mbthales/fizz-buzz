5fa3c1429d51eda7fa1af8e23763a486eddc2330
Thales Maia <mbthales10@gmail.com>
2023-01-12 18:27:58 -0300

    How to play component remade

--- src/Components/HowToPlay.jsx
+++ src/Components/HowToPlay.jsx
@@ -9,9 +9,14 @@ const HowToPlay = () => {
 			{(() =>{
 				if(helpAvailable){
 					return (						
-						<div className="text-center text-xl absolute bg-accent text-primary-600 top-20% h-64 p-10 flex items-center">
-							<p>Se o número for múltiplo de 3, aperte o botão Fizz. Se o botão for múltiplo de 5, aperte o botão Buzz. Se ele for múltiplo de 15, aperte o botão FizzBuzz.
-							</p>
+						<div className="text-center text-lg absolute bg-accent text-primary-600 h-96 w-full p-10 flex flex-col justify-center center-vertically rounded">
+							<h2 className="text-3xl font-semibold">Como jogar</h2>
+							<ul className="my-8">
+								<li>Número múltiplo de 3: <span className="font-bold">Fizz</span></li>
+								<li>Número múltiplo de 5: <span className="font-bold">Buzz</span></li>
+								<li>Número múltiplo de 15: <span className="font-bold">FizzBuzz</span></li>
+							</ul>
+							<p>O número máximo é 20.</p>
 						</div>
 					);
 				}
