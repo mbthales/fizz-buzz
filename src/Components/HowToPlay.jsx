3e1e47cba91924a04a8d96f77d6fd1dcc1ad26d8
Thales Maia <mbthales10@gmail.com>
2023-01-12 22:45:10 -0300

    Change how to play text

--- src/Components/HowToPlay.jsx
+++ src/Components/HowToPlay.jsx
@@ -8,9 +8,9 @@ const HowToPlayText = () => {
 			transition={{ease: easeIn, duration: 0.1}} className="text-center text-lg absolute bg-accent text-primary-600 h-96 w-full p-10 flex flex-col justify-center center-vertically rounded">
 			<h2 className="text-3xl font-semibold">Como jogar</h2>
 			<ul className="my-8">
-				<li>Número múltiplo de 3: <span className="font-bold">Fizz</span></li>
-				<li>Número múltiplo de 5: <span className="font-bold">Buzz</span></li>
-				<li>Número múltiplo de 15: <span className="font-bold">FizzBuzz</span></li>
+				<li>Número divisível por 3: <span className="font-bold">Fizz</span></li>
+				<li>Número divisível por 5: <span className="font-bold">Buzz</span></li>
+				<li>Número divisível por 15: <span className="font-bold">FizzBuzz</span></li>
 			</ul>
 			<p>O número máximo é 20.</p>
 		</motion.div>
@@ -23,11 +23,9 @@ const HowToPlay = () => {
 
 	return (
 		<>
-			
 			<AnimatePresence>
 				{ helpAvailable && <HowToPlayText /> }
 			</AnimatePresence>
-			
 			<motion.button whileHover={{ scale: 1.5 }} whileTap={{ rotate: 360 }}
 				className="absolute top-negative-3rem right-4 text-xl font-bold text-accent" type="button" onClick={() => setHelpAvailable(!helpAvailable)}>
 				?
